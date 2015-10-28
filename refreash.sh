1c2a7a4eb1664538c775167c7b53de7d6c86d193
Carey <ziyunhx@gmail.com>
2015-10-28 15:19:26 +0800

    add docker refresh bash.

---
+++ refreash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker pull ziyunhx/base
+docker pull ziyunhx/zookeeper
+docker pull ziyunhx/storm
+docker pull ziyunhx/storm-nimbus
+docker pull ziyunhx/storm-supervisor
+docker pull ziyunhx/storm-ui
