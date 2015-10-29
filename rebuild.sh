68e703dec267b234b550cbe67fb4570e12a7630e
Carey <ziyunhx@gmail.com>
2015-10-29 17:36:40 +0800

    update something.

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-docker build -t="ziyunhx/base" base
 docker build -t="ziyunhx/zookeeper" zookeeper
-docker build -t="ziyunhx/storm" storm
 docker build -t="ziyunhx/storm-nimbus" storm-nimbus
 docker build -t="ziyunhx/storm-supervisor" storm-supervisor
 docker build -t="ziyunhx/storm-ui" storm-ui
--- refresh.sh
+++ refresh.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-docker pull ziyunhx/base
 docker pull ziyunhx/zookeeper
-docker pull ziyunhx/storm
 docker pull ziyunhx/storm-nimbus
 docker pull ziyunhx/storm-supervisor
 docker pull ziyunhx/storm-ui
